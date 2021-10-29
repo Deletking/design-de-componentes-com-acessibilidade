6196acb170da338c842db552de7ecf054cb75f90
Paulo Hericles <paulo.hericles@squadra.com.br>
2021-10-29 12:49:26 -0300

    add style in button-yes-no

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-yes-no-button-group',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class YesNoButtonGroupComponent implements OnInit {
 
+  @Input() public value: String = null;
+  @Input() public label: String = '';
+  
   constructor() { }
 
   ngOnInit(): void {
